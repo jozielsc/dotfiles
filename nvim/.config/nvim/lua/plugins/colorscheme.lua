997caea8257580b705067523ef2867aa2cfed439
Joziel Costa <jozielsc@gmail.com>
2026-09-02 23:08:09 -0300

    feat: adiciona conform para tratar formataçao, fix e ordenaçao de import com ruff

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,35 @@
 return {
-	-- Importa a suíte Nightfox
-	{
-		"EdenEast/nightfox.nvim",
-		priority = 1000,
-		opts = {
-			options = {
-				-- Melhora a renderização de cores no terminal/tmux
-				terminal_colors = true,
-				styles = {
-					comments = "italic",
-					keywords = "bold",
-					types = "bold",
-				},
-				-- Ajuste específico para a tela do X61s:
-				-- Podemos forçar o fundo para preto absoluto (#000000)
-				-- ou deixar no padrão do carbonfox (que já é um cinza/preto super escuro #161616)
-				palettes = {
-					carbonfox = {
-						-- Descomente a linha abaixo se quiser o fundo 100% preto
-						-- bg1 = "#000000",
-					},
-				},
-			},
-		},
-	},
+  -- Importa a suíte Nightfox
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+    opts = {
+      options = {
+        -- Melhora a renderização de cores no terminal/tmux
+        terminal_colors = true,
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "bold",
+        },
+        -- Ajuste específico para a tela do X61s:
+        -- Podemos forçar o fundo para preto absoluto (#000000)
+        -- ou deixar no padrão do carbonfox (que já é um cinza/preto super escuro #161616)
+        palettes = {
+          carbonfox = {
+            -- Descomente a linha abaixo se quiser o fundo 100% preto
+            -- bg1 = "#000000",
+          },
+        },
+      },
+    },
+  },
 
-	-- Define o carbonfox como o tema principal do LazyVim
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "carbonfox",
-		},
-	},
+  -- Define o carbonfox como o tema principal do LazyVim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
 }
